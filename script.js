7e6d58a56bd5ee3facf201b5b8aaf37980a6e39f
vuksavic <vuk.savic@jolacgi.com>
2025-01-16 14:51:37 +0100

    cleanup, camera type selection interface

--- script.js
+++ script.js
@@ -1,127 +1,106 @@
-import * as THREE from 'three';
-import Stats from 'three/addons/libs/stats.module.js';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import { RoomEnvironment } from 'three/addons/environments/RoomEnvironment.js';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
+import * as THREE from 'three'
+import Stats from 'three/addons/libs/stats.module.js'
+import { OrbitControls } from 'three/examples/jsm/Addons.js'
+import { RoomEnvironment } from 'three/examples/jsm/Addons.js'
+import { GLTFLoader } from 'three/examples/jsm/Addons.js'
+import GUI from 'lil-gui'
 
-const clock = new THREE.Clock();
-const container = document.getElementById( 'container' );
+const container = document.getElementById( 'container' )
 
-const stats = new Stats();
-container.appendChild( stats.dom );
+const gui = new GUI()
 
-const renderer = new THREE.WebGLRenderer( { alpha: true, antialias: true });
-renderer.setPixelRatio( window.devicePixelRatio );
-renderer.setSize( window.innerWidth, window.innerHeight );
-container.appendChild( renderer.domElement );
+const stats = new Stats()
+stats.showPanel(0)
+container.appendChild( stats.dom )
 
-const pmrremGenerator = new THREE.PMREMGenerator( renderer );
+const renderer = new THREE.WebGLRenderer( { alpha: true, antialias: true })
+renderer.setPixelRatio( window.devicePixelRatio )
+renderer.setSize( window.innerWidth, window.innerHeight )
+container.appendChild( renderer.domElement )
 
-const scene = new THREE.Scene();
-scene.background = new THREE.Color( 0xbfe3dd );
-scene.environment = pmrremGenerator.fromScene( new RoomEnvironment(), 0.04 ).texture;
-
-const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 1000 );
-camera.position.set( 3, 3, 0 );
+const pmrremGenerator = new THREE.PMREMGenerator( renderer )
 
 function dumpObject(obj, lines = [], isLast = true, prefix = '') {
-    const localPrefix = isLast ? '└─' : '├─';
-    lines.push(`${prefix}${prefix ? localPrefix : ''}${obj.name || '*no-name*'} [${obj.type}]`);
-    const newPrefix = prefix + (isLast ? '  ' : '│ ');
-    const lastNdx = obj.children.length - 1;
+    const localPrefix = isLast ? '└─' : '├─'
+    lines.push(`${prefix}${prefix ? localPrefix : ''}${obj.name || '*no-name*'} [${obj.type}]`)
+    const newPrefix = prefix + (isLast ? '  ' : '│ ')
+    const lastNdx = obj.children.length - 1
     obj.children.forEach((child, ndx) => {
-      const isLast = ndx === lastNdx;
-      dumpObject(child, lines, isLast, newPrefix);
-    });
-    return lines;
-  }
+        const isLast = ndx === lastNdx
+        dumpObject(child, lines, isLast, newPrefix)
+    })
+    return lines
+}
 
-const controls = new OrbitControls( camera, renderer.domElement );
-controls.target.set( 0, 0.5, 0 );
-controls.update();
-controls.enablePan = true;
-controls.enableDamping = true;
-
-let modelX = 1;
-let modelY = 1;
-let modelZ = 1;
+const scene = new THREE.Scene()
+scene.background = new THREE.Color( 0xbfe3dd )
+scene.environment = pmrremGenerator.fromScene( new RoomEnvironment(), 0.04 ).texture
 
 const loader = new GLTFLoader();
 loader.load( 'assets/herman_miller_eames_lounge_chair/scene.gltf', function ( gltf ) {
-    
     const model = gltf.scene;
     model.position.set( 0, 0, 0 );
-    model.scale.set( modelX, modelY, modelZ );
+    model.scale.set( 1, 1, 1 );
     scene.add( model );
-    renderer.setAnimationLoop( animate );
     console.log(dumpObject(model).join('\n'));
-
-}, undefined, function ( error ) {
     
-    console.error( error );
-
+}, undefined, function ( error ) {
+    console.error( error );    
 } );
+
+const sizes = {
+    width: 800,
+    height: 600
+}
+const aspectRatio = sizes.width / sizes.height
+
+const perspCamera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.001, 1000 )
+const orthoCamera = new THREE.OrthographicCamera(-1 * aspectRatio, 1 * aspectRatio, 1, -1, 0.01, 100000)
+let camera = perspCamera
+gui.add(camera, 'camera', ['perspective', 'orthographic'] ).onChange(function(value) {
+    if (value === 'orthographic') {
+        camera = orthoCamera
+    } else {
+        camera = perspCamera
+    }
+    controls.object = camera
+    controls.update()
+})
+camera.position.set( 3, 3, 0 )
+
+scene.add( camera )
+const controls = new OrbitControls( camera, renderer.domElement )
+controls.target.set( 0, 0.5, 0 )
+controls.update()
+controls.enablePan = true
+controls.enableDamping = true
+
 
 window.onresize = function() {
     
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-
-    renderer.setSize( window.innerWidth, window.innerHeight );
-
-}
-
-function animate() {
+    camera.aspect = window.innerWidth / window.innerHeight
+    camera.updateProjectionMatrix()
     
-    const delta = clock.getDelta();
-    controls.update( delta );
-    stats.update();
-    renderer.render( scene, camera );
-
+    renderer.setSize( window.innerWidth, window.innerHeight )
 }
 
-const gui = new GUI();
+window.addEventListener('resize', () => {
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
 
-const myObject = {
-	myBoolean: true,
-	myFunction: function() { console.log( 'Button Clicked!' ); },
-	myString: 'lil-gui',
-	myNumber: 1,
-    color1: '#ff0000',
-};
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
 
-gui.add( myObject, 'myBoolean' );  // Checkbox
-gui.add( myObject, 'myFunction' ); // Button
-gui.add( myObject, 'myString' );   // Text Field
-gui.add( myObject, 'myNumber' );   // Number Field
-gui.add( myObject, 'color1' );     // Color Field
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
 
-gui.add( modelX, 'X');
-gui.add( modelY, 'Y');
-gui.add( modelZ, 'Z');
+const clock = new THREE.Clock()
 
-// Add sliders to number fields by passing min and max
-gui.add( myObject, 'myNumber', 0, 1 );
-gui.add( myObject, 'myNumber', 0, 100, 2 ); // snap to even numbers
+const tick = () => {
+    stats.update()
+    renderer.render( scene, camera )
+    window.requestAnimationFrame(tick)
+}
 
-// Create dropdowns by passing an array or object of named values
-gui.add( myObject, 'myNumber', [ 0, 1, 2 ] );
-gui.add( myObject, 'myNumber', { Label1: 0, Label2: 1, Label3: 2 } );
-
-// Chainable methods
-gui.add( myObject, 'myProperty' )
-	.name( 'Custom Name' )
-	.onChange( value => {
-		console.log( value );
-	} );
-
-// Create color pickers for multiple color formats
-const colorFormats = {
-	string: '#ffffff',
-	int: 0xffffff,
-	object: { r: 1, g: 1, b: 1 },
-	array: [ 1, 1, 1 ]
-};
-
-gui.addColor( colorFormats, 'string' );
+tick()
